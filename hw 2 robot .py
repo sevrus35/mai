acfa2431f98ff1bb14d4d1266c16a18ea67b90a9
sevrus35 <161660602+sevrus35@users.noreply.github.com>
2024-03-29 02:52:47 +0300

    Add files via upload

---
+++ hw 2 robot .py
@@ -0,0 +1,11 @@
+Power switch is off.
+Diesel engine is turned on.
+Robot is moving forward.
+Robot is rotating 90 degrees.
+Power switch is turned off.
+Robot has stopped.
+Robot has stopped.
+
+
+** Process exited - Return Code: 0 **
+Press Enter to exit terminal
